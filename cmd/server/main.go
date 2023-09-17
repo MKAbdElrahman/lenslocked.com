e8834d4d08703f2e50c10f6e77cb1f75e2719c67
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2023-09-17 03:31:58 +0300

    added custom 404 page

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,6 +16,9 @@ func main() {
 	router.HandleFunc("/contact", contact)
 	router.HandleFunc("/faq", faq)
 
+	router.NotFoundHandler = http.HandlerFunc(notFound)
+
+	
 	addr := ":8080"
 	log.Printf("Starting server on %s", addr)
 	http.ListenAndServe(addr, router)
@@ -23,16 +26,9 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you "+
-			"were looking for :(</h1>"+
-			"<p>Please email us if you keep being sent to an "+
-			"invalid page.</p>")
-		return
-	}
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
+
 func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>To get in touch, please send an email "+
 		"to <a href=\"mailto:support@lenslocked.com\">"+
@@ -41,4 +37,12 @@ func contact(w http.ResponseWriter, r *http.Request) {
 
 func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>FAQ</h1>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We could not find the page you "+
+		"were looking for :(</h1>"+
+		"<p>Please email us if you keep being sent to an "+
+		"invalid page.</p>")
 }
